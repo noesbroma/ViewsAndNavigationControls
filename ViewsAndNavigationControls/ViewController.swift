69c2728641dc70b7da7f32825b7845314a355ef3
Judit <noesbroma@hotmail.com>
2024-02-26 19:40:31 +0100

    Initial Commit

---
+++ ViewsAndNavigationControls/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ViewsAndNavigationControls
+//
+//  Created by Judith Leon on 26/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myFakeView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func myButtonAction(_ sender: Any) {
+        myFakeView.isHidden = true
+    }
+    
+}
+
