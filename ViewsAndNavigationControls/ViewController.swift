4cca014a9d913c07b223f62d1c66a6c8ea7ce4de
Judit <noesbroma@hotmail.com>
2024-04-05 13:35:24 +0200

    Add scrollView, mapView && webView

--- ViewsAndNavigationControls/ViewController.swift
+++ ViewsAndNavigationControls/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import WebKit
+import MapKit
 
 class ViewController: UIViewController {
 
@@ -13,10 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var myView: UIView!
     @IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var myFakeView: UIView!
+    @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var myMap: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myWebView.load(URLRequest(url: URL(string: "https://google.es")!))
+        
+        myMap.centerCoordinate = CLLocationCoordinate2D(latitude: 42.6074875, longitude: -8.3132621)
     }
 
 
